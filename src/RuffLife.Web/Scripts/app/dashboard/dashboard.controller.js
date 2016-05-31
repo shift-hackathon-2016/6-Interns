5ce3d6b2417bc5294fe276a437748b7eaa9652e6
Izabela Domazet <v-izabela.domazet@dump.hr>
2016-05-31 14:54:10 +0200

    repository added

---
+++ src/RuffLife.Web/Scripts/app/dashboard/dashboard.controller.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+angular.module('app').controller('dashboardController', DashboardController);
+
+UserRepository.$inject = ['userRepository'];
+
+function DashboardController(userRepository, $stateParams) {
+    var vm = this;
+    //var username = $stateParams.username;
+
+    userRepository
+    .get(username)
+    .then(function (user) {
+        vm.user = user;
+    });
+    
+};
