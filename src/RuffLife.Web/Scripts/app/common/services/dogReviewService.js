61c15a61de24d61c52e1f7efb09b7bd5e1aaa66d
Josipa Braica <josipa@dump.hr>
2016-05-31 23:54:40 +0200

    added services

---
+++ src/RuffLife.Web/Scripts/app/common/services/dogReviewService.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+angular.module('app').factory('dogReviewService', DogReviewService);
+
+DogReviewService.$inject = ['$http', '$q'];
+
+function DogReviewService($http, $q) {
+    var dogReviews;
+
+    return {
+        dogReviews: dogReviews
+    };
+};
---
+++ src/RuffLife.Web/Scripts/app/common/services/dogService.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+angular.module('app').factory('dogService', DogService);
+
+DogService.$inject = ['$http', '$q'];
+
+function DogService($http, $q) {
+
+    return {
+    };
+};
