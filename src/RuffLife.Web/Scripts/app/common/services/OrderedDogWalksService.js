ca59890584beeadb444f2c409528d553253f8d54
Izabela Domazet <v-izabela.domazet@dump.hr>
2016-06-01 01:45:18 +0200

    added services

---
+++ src/RuffLife.Web/Scripts/app/common/services/OrderedDogWalksService.js
@@ -0,0 +1,34 @@
+﻿'use strict';
+
+angular.module('app').factory('orderedDogWalksService', OrderedDogWalksService);
+
+OrderedDogWalksService.$inject = ['$http', '$q'];
+
+function OrderedDogWalksService($http, $q) {
+    var orderedWalks = [{ startTime: new Date(), endTime: new Date(), location: 'Split', price: '50', walker: { username: 'Mirko' } },
+        { startTime: new Date(), endTime: new Date(), location: 'Kaštela', price: '10', walker: { username: 'Nikola' } }];
+
+    /*function addUpcomingWalk(walk) {
+        upcomingWalks.push(walk);
+    };*/
+
+    function getUpcomingWalks() {
+        return $q.resolve(orderedWalks);
+    };
+
+    function getCurrentWalk() {
+        angular.forEach(orderedWalks, function (value, key) {
+            var currentTime = Date.now();
+            if (!(value.endTime > currentTime && value.startTime < currentTime))
+                return $q.resolve('');
+            return $q.resolve(value[key]);
+        });
+    };
+
+    return {
+        orderedWalks: orderedWalks,
+        getUpcomingWalks: getUpcomingWalks,
+        getCurrentWalk: getCurrentWalk,
+        //addUpcomingWalk: addUpcomingWalk
+    };
+};
---
+++ src/RuffLife.Web/Scripts/app/common/services/pastDogWalksService.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+angular.module('app').factory('pastDogWalksService', PastDogWalksService);
+
+PastDogWalksService.$inject = ['$http', '$q'];
+
+function PastDogWalksService($http, $q) {
+    var orderedWalks = [{ startTime: new Date(), endTime: new Date(), location: 'Split', price: '50', walker: { username: 'Mirko' } },
+        { startTime: new Date(), endTime: new Date(), location: 'Kaštela', price: '10', walker: { username: 'Nikola' } }];
+
+    var pastWalks = [];
+
+    function getPastWalks() {
+        angular.forEach(orderedWalks, function (value, key) {
+            var currentTime = Date.now();
+            if (value.endTime < currentTime)
+                pastWalks.push(value);
+        });
+        return $q.resolve(pastWalks);
+    };
+
+    return {
+        getPastWalk: getPastWalks,
+    };
+};
