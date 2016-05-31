c12f2afbef4e1b93b4cce567ead329906275bd5f
wr9 <asikim00@fesb.hr>
2016-05-31 13:06:37 +0200

    added walk offer

---
+++ src/RuffLife.Data/Models/WalkOffer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuffLife.Data.Models
+{
+    public class WalkOffer:BaseModel
+    {
+        public bool IsActive { get; set; }
+
+        public virtual Walk Walk { get; set; }
+        public virtual Walker Walker { get; set; }
+    }
+}
