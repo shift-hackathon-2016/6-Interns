07aaa46ca2181cd7b65ef2ce8668c7acf703df96
Mirko Bojcic <bojcicm@gmail.com>
2016-05-31 10:33:43 +0200

    init data layer

---
+++ src/RuffLife.Data/Context/RuffLifeContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace RuffLife.Data.Context
+{
+    public class RuffLifeContext : DbContext
+    {
+        public RuffLifeContext()
+            :base("RuffLifeContext")
+        {
+            
+        }
+    }
+}
---
+++ src/RuffLife.Data/Models/BaseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace RuffLife.Data.Models
+{
+    public class BaseModel
+    {
+        public int Id { get; set; }
+    }
+}
