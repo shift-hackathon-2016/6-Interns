867feabb2175d40ef2b9f5f2b528907ca92a84e4
Mirko Bojcic <bojcicm@gmail.com>
2016-05-31 12:18:11 +0200

    added context configurations

---
+++ src/RuffLife.Data/Context/Configurations/WalkerConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using RuffLife.Data.Models;
+
+namespace RuffLife.Data.Context.Configurations
+{
+    public class WalkerConfiguration : EntityTypeConfiguration<Walker>
+    {
+        public WalkerConfiguration()
+        {
+            HasMany(w => w.ReviewsReceived)
+                .WithRequired(r => r.Walker)
+                .WillCascadeOnDelete();
+
+            HasMany(w => w.ReviewsGiven)
+                .WithRequired(r => r.Walker)
+                .WillCascadeOnDelete();
+
+            HasMany(w => w.Walks)
+                .WithRequired(walk => walk.Walker)
+                .WillCascadeOnDelete();
+
+        }
+    }
+}
