16ef7b9e86658a1cff142ea526c8fd818e09eb00
Mirko Bojcic <bojcicm@gmail.com>
2016-05-31 18:02:52 +0200

    implemented walker repository, service and controller

---
+++ src/RuffLife.Core/Services/WalkerService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RuffLife.Core.Models.Walker;
+using RuffLife.Core.Repositories.Interfaces;
+using RuffLife.Core.Services.Interfaces;
+
+namespace RuffLife.Core.Services
+{
+    public class WalkerService : IWalkerService
+    {
+        private readonly IWalkerRepository _walkerRepository;
+
+        public WalkerService(IWalkerRepository walkerRepository)
+        {
+            _walkerRepository = walkerRepository;
+        }
+
+        public ViewWalkerDto GetWalker(int walkerId)
+        {
+            var walker = _walkerRepository.GetWalker(walkerId);
+            return walker;
+        }
+
+        public void Dispose()
+        {
+            _walkerRepository.Dispose();
+        }
+    }
+}
---
+++ src/RuffLife.Web/Controllers/API/WalkerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RuffLife.Core.Services.Interfaces;
+
+namespace RuffLife.Web.Controllers.API
+{
+    [RoutePrefix("walker")]
+    public class WalkerController : ApiController
+    {
+        private readonly IWalkerService _walkerService;
+
+        public WalkerController(IWalkerService walkerService)
+        {
+            _walkerService = walkerService;
+        }
+
+        [Route("get-single/{id}")]
+        [HttpGet]
+        public IHttpActionResult GetSingleWalker(int id)
+        {
+            var user = _walkerService.GetWalker(id);
+            if (user != null)
+                return Ok(user);
+            return BadRequest("Walker with that Id doesnt exist");
+        }
+    }
+}
