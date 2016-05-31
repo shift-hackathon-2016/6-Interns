719b8aa3467267c9955bef63f8a5845a6c7722c3
Josipa Braica <josipa@dump.hr>
2016-06-01 00:46:34 +0200

    added calls

--- src/RuffLife.Web/Scripts/app/common/services/dogReviewService.js
+++ src/RuffLife.Web/Scripts/app/common/services/dogReviewService.js
@@ -7,7 +7,16 @@ DogReviewService.$inject = ['$http', '$q'];
 function DogReviewService($http, $q) {
     var dogReviews;
 
+    function getDogReviews(id) {
+        $http.get('/api/reviewForDogs/get-reviews/' + id).then(function (response) {
+            dogReviews = response.data;
+            console.log(dogReviews);
+            return $q.resolve(dogReviews);
+        });
+    };
+
     return {
-        dogReviews: dogReviews
+        dogReviews: dogReviews,
+        getDogReviews: getDogReviews
     };
 };
--- src/RuffLife.Web/Scripts/app/common/services/walkerReviewService.js
+++ src/RuffLife.Web/Scripts/app/common/services/walkerReviewService.js
@@ -7,7 +7,16 @@ WalkerReviewService.$inject = ['$http', '$q'];
 function WalkerReviewService($http, $q) {
     var walkerReviews;
 
+    function getWalkerReviews(id) {
+        $http.get('/api/reviewForWalkers/' + id).then(function () {
+            console.log('walker reviews: ', response.data);
+            walkerReviews = response.data;
+            return $q.resolve(walkerReviews);
+        });
+    }
+
     return {
-        walkerReviews: walkerReviews
+        walkerReviews: walkerReviews,
+        getWalkerReviews: getWalkerReviews
     };
 };
