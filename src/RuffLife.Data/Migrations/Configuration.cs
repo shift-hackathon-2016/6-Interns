a18f783c370b9a19dbad74b39687675720b095d8
wr9 <asikim00@fesb.hr>
2016-05-31 16:17:24 +0200

    fixing seed

--- src/RuffLife.Data/Migrations/Configuration.cs
+++ src/RuffLife.Data/Migrations/Configuration.cs
@@ -23,16 +23,16 @@ namespace RuffLife.Data.Migrations
                 new Owner { Username = "Mirko", Location = "Kaštela", Address = "Mirkova adresa", ContactNumber = "095", Email = "mirko@dump.hr" },
                 new Owner { Username = "Nikola", Location = "Split", Address = "Nikolina adresa", ContactNumber = "095", Email = "nikola@dump.hr" }
             };
-            owners.ForEach(x => context.Owners.Add(x));
+            owners.ForEach(x => context.Owners.AddOrUpdate(_ => _.Username, x));
             context.SaveChanges();
 
             var dogs = new List<Dog>
             {
-                new Dog { Name = "Dota", Breed = "Jack Russell", Age = "7", Gender = 'F', Owner = context.Owners.FirstOrDefault(u => u.Username == "Andrea")},
-                new Dog { Name = "Alba", Breed = "Jack Russell", Age = "2", Gender = 'F', Owner = context.Owners.FirstOrDefault(u => u.Username == "Nikola") },
-                new Dog { Name = "Vau", Breed = "Labrador", Age = "5", Gender = 'M', Owner = context.Owners.FirstOrDefault(u => u.Username == "Mirko") }
+                new Dog { Name = "Dota", Breed = "Jack Russell", Age = "7", Gender = 'F', Owner = context.Owners.FirstOrDefault(x => x.Username == "Andrea")},
+                new Dog { Name = "Alba", Breed = "Jack Russell", Age = "2", Gender = 'F', Owner = context.Owners.FirstOrDefault(x => x.Username == "Nikola") },
+                new Dog { Name = "Vau", Breed = "Labrador", Age = "5", Gender = 'M', Owner = context.Owners.FirstOrDefault(x => x.Username == "Mirko") }
             };
-            dogs.ForEach(x => context.Dogs.Add(x));
+            dogs.ForEach(x => context.Dogs.AddOrUpdate(_ => new { _.Owner, _.Name }, x));
             context.SaveChanges();
 
             var walkers = new List<Walker>
@@ -41,7 +41,7 @@ namespace RuffLife.Data.Migrations
                 new Walker { Username = "Josipa", Location = "Kaštela", Email = "josipa@dump.hr", CostPerHourInHRK = 60, ContactNumber = "095" },
                 new Walker { Username = "Ante", Location = "Zagreb", Email = "ante@mail.com", CostPerHourInHRK = 70, ContactNumber = "095" }
             };
-            walkers.ForEach(x => context.Walkers.Add(x));
+            walkers.ForEach(x => context.Walkers.AddOrUpdate(_ => _.Username, x));
             context.SaveChanges();
 
             var walks = new List<Walk>
@@ -50,7 +50,7 @@ namespace RuffLife.Data.Migrations
                 new Walk { StartTime = new DateTime(2016, 5, 31, 10, 0, 0), EndTime = new DateTime(2016, 5, 31, 12, 0, 0), Location = "Lokacija2", Price = 60, Walker = context.Walkers.FirstOrDefault(x => x.Username == "Josipa") },
                 new Walk { StartTime = new DateTime(2016, 5, 31, 20, 0, 0), EndTime = new DateTime(2016, 5, 31, 23, 0, 0), Location = "Lokacija3", Price = 70, Walker = context.Walkers.FirstOrDefault(x => x.Username == "Izabela") }
             };
-            walks.ForEach(x => context.Walks.Add(x));
+            walks.ForEach(x => context.Walks.AddOrUpdate(_ => new { _.Walker, _.Dogs, _.StartTime }, x));
             context.SaveChanges();
 
             var reviewForWalkers = new List<ReviewForWalker>
@@ -59,7 +59,7 @@ namespace RuffLife.Data.Migrations
                 new ReviewForWalker { Grade = 4, Review = "Vrlo dobro", Walker = context.Walkers.FirstOrDefault(x => x.Username == "Josipa"), Owner = context.Owners.FirstOrDefault(x => x.Username == "Mirko") },
                 new ReviewForWalker { Grade = 3, Review = "OK", Walker = context.Walkers.FirstOrDefault(x => x.Username == "Ante"), Owner = context.Owners.FirstOrDefault(x => x.Username == "Nikola") }
             };
-            reviewForWalkers.ForEach(x => context.ReviewsForWalkers.Add(x));
+            reviewForWalkers.ForEach(x => context.ReviewsForWalkers.AddOrUpdate(_ => new { _.Walker, _.Owner }, x));
             context.SaveChanges();
 
             var reviewForDogs = new List<ReviewForDog>
@@ -68,7 +68,7 @@ namespace RuffLife.Data.Migrations
                 new ReviewForDog { Grade = 5, Review = "Super", Walker = context.Walkers.FirstOrDefault(x => x.Username == "Josipa"), Dog = context.Dogs.FirstOrDefault(x => x.Name == "Alba") },
                 new ReviewForDog { Grade = 4, Review = "Vrlo dobro", Walker = context.Walkers.FirstOrDefault(x => x.Username == "Ante"), Dog = context.Dogs.FirstOrDefault(x => x.Name == "Vau") }
             };
-            reviewForDogs.ForEach(x => context.ReviewsForDogs.Add(x));
+            reviewForDogs.ForEach(x => context.ReviewsForDogs.AddOrUpdate(_ => new { _.Dog, _.Walker }, x));
             context.SaveChanges();
         }
     }
--- src/RuffLife.Web/App_Start/Mapping/AutoMapperConfig.cs
+++ src/RuffLife.Web/App_Start/Mapping/AutoMapperConfig.cs
@@ -3,6 +3,7 @@ using RuffLife.Core.Models.Walker;
 using RuffLife.Core.Repositories;
 using RuffLife.Data.Models;
 using AutoMapper;
+using RuffLife.Core.Models.Walk;
 
 namespace RuffLife.Web.Mapping
 {
@@ -27,7 +28,7 @@ namespace RuffLife.Web.Mapping
             Mapper.CreateMap<Owner, ViewOwnerDto>();
             Mapper.CreateMap<ViewOwnerDto, Owner>();
             Mapper.CreateMap<Walker, ViewWalkerDto>();
-            Mapper.CreateMap<Walk, ViewWalkerDto>();
+            Mapper.CreateMap<Walk, ViewWalkDto>();
         }
     }
 }
