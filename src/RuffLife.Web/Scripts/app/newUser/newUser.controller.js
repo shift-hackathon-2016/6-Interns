79c7d181996d7c724094e895f75f0b04dc4a70a2
Josipa Braica <josipa@dump.hr>
2016-05-31 16:11:05 +0200

    minor fixes

---
+++ src/RuffLife.Web/Scripts/app/newUser/newUser.controller.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+angular.module('app').controller('newUserController', NewUserController);
+
+NewUserController.$inject = ['$state', 'userAuthService'];
+
+function NewUserController($state, userAuthService) {
+    var vm = this;
+
+    vm.user = {};
+
+    vm.userType;
+    userAuthService.getNewUserType().then(function (type) {
+        vm.userType = type;
+    });
+
+    vm.save = function () {
+        return $state.go('dashboard');
+    };
+};
---
+++ src/RuffLife.Web/Scripts/app/newUser/newUser.state.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+angular.module('app').config(NewUserStateProvider);
+
+NewUserStateProvider.$inject = ['$stateProvider'];
+
+function NewUserStateProvider($stateProvider) {
+    $stateProvider
+    .state('newUser', {
+        url: '/new-user',
+        templateUrl: './../../Scripts/app/newUser/newUser.template.html',
+        controller: 'newUserController',
+        controllerAs: 'vm'
+    });
+};
