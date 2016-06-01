386214127e9ea4a44d0e25bf686ac65ca2e73e47
wr9 <asikim00@fesb.hr>
2016-06-01 03:15:46 +0200

    walk offer progress and fixed walker binding bugs

--- src/RuffLife.Core/Repositories/Interfaces/IWalkOfferRepository.cs
+++ src/RuffLife.Core/Repositories/Interfaces/IWalkOfferRepository.cs
@@ -2,14 +2,15 @@
 using RuffLife.Core.Models.Walker;
 using RuffLife.Core.Models.WalkOffer;
 using RuffLife.Data.Models;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace RuffLife.Core.Repositories.Interfaces
 {
     public interface IWalkOfferRepository : IDisposable
     {
-        WalkOffer CreateWalkOffer(CreateWalkOfferDto walkOffer);
-        void VoteOnOffer(ViewWalkerDto walkerThatVoted, WalkOffer votedOffer);
-        void LockOffer(WalkOffer walkOfferToLock);
-        WalkOffer GetOfferFromRepo(int walkOfferId);
+        void CreateWalkOffer(CreateWalkOfferDto newWalkOffer);
+        void UpdateWalkOffer(UpdateWalkOfferDto updatedWalkOffer);
+        IList<ViewWalkOfferDto> GetWalkOffersByWalk(int walkId);
     }
 }
